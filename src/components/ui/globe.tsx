7478a11ffc6003ad045f892ed7272e5203787157
brahimbouargane <brahim.bouargane2000@gmail.com>
2024-11-29 17:24:15 +0100

    update

--- src/components/ui/globe.tsx
+++ src/components/ui/globe.tsx
@@ -247,34 +247,35 @@ export function World(props: WorldProps) {
   const scene = new Scene();
   scene.fog = new Fog(0xffffff, 400, 2000);
   return (
-    <Canvas scene={scene} camera={new PerspectiveCamera(50, aspect, 180, 1800)}>
-      <WebGLRendererConfig />
-      <ambientLight color={globeConfig.ambientLight} intensity={0.6} />
-      <directionalLight
-        color={globeConfig.directionalLeftLight}
-        position={new Vector3(-400, 100, 400)}
-      />
-      <directionalLight
-        color={globeConfig.directionalTopLight}
-        position={new Vector3(-200, 500, 200)}
-      />
-      <pointLight
-        color={globeConfig.pointLight}
-        position={new Vector3(-200, 500, 200)}
-        intensity={0.8}
-      />
-      <Globe {...props} />
-      <OrbitControls
-        enablePan={false}
-        enableZoom={false}
-        minDistance={cameraZ}
-        maxDistance={cameraZ}
-        autoRotateSpeed={2}
-        autoRotate={false}
-        minPolarAngle={Math.PI / 3.5}
-        maxPolarAngle={Math.PI - Math.PI / 3}
-      />
-    </Canvas>
+    // <Canvas scene={scene} camera={new PerspectiveCamera(50, aspect, 180, 1800)}>
+    //   <WebGLRendererConfig />
+    //   <ambientLight color={globeConfig.ambientLight} intensity={0.6} />
+    //   <directionalLight
+    //     color={globeConfig.directionalLeftLight}
+    //     position={new Vector3(-400, 100, 400)}
+    //   />
+    //   <directionalLight
+    //     color={globeConfig.directionalTopLight}
+    //     position={new Vector3(-200, 500, 200)}
+    //   />
+    //   <pointLight
+    //     color={globeConfig.pointLight}
+    //     position={new Vector3(-200, 500, 200)}
+    //     intensity={0.8}
+    //   />
+    //   <Globe {...props} />
+    //   <OrbitControls
+    //     enablePan={false}
+    //     enableZoom={false}
+    //     minDistance={cameraZ}
+    //     maxDistance={cameraZ}
+    //     autoRotateSpeed={2}
+    //     autoRotate={false}
+    //     minPolarAngle={Math.PI / 3.5}
+    //     maxPolarAngle={Math.PI - Math.PI / 3}
+    //   />
+    // </Canvas>
+    <div></div>
   );
 }
 
